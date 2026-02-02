053bd738a7ebc03eecb048f0a21641ac64d0d480
Ryan-Gamin <134164090+Ryan-Gamin@users.noreply.github.com>
2026-02-02 00:03:48 +0000

    Add configuration settings to public/config.js
    Initialize _CONFIG with backend and settings.

--- public/config.js
+++ public/config.js
@@ -1,1 +1,9 @@
-let _CONFIG = {};
+let _CONFIG = {
+    // This points the proxy to a working public backend
+    bare: 'https://tomp.app/',
+    
+    // These are standard settings to keep it running smoothly
+    prefix: '/service/',
+    encodeUrl: x => x,
+    decodeUrl: x => x,
+};
